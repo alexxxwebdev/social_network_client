55205caf18a8e529d49dbde29798ac886a9a72c6
Alexander Tyulin <alexgtr9630@mail.ru>
2020-11-17 20:45:38 +0300

    PRJ-SOC-2: test commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,32 +4,7 @@ import styles from './App.styl'
 
 function App() {
   return (
-    <div className={styles.app}>
-      <header className={styles.header}>
-        <img
-          src={logo}
-          className={styles.logo}
-          alt='logo'
-        />
-        <p>
-          Edit
-          {' '}
-          <code>
-            src/App.tsx
-          </code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div></div>
   )
 }
 
